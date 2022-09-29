3d47f8e1ca613327b088b991df5d35745b6dc07a
reinaldocosta <reinaldo.costa@gentrop.com>
2022-09-29 12:46:26 -0300

    feat: list cfops

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "../deps.ts";
+import { makeGetCfopController, makeListCfopController } from "../usecases/cfop/index.ts";
 import { makeGetNcmController, makeNcmController } from "../usecases/ncm/index.ts";
 import { adaptOakRoute } from "./ControllerAdapter.ts";
 
@@ -7,8 +8,12 @@ const router = new Router();
 router.get('/ncm', adaptOakRoute(makeNcmController()))
 router.get('/ncm/:code', adaptOakRoute(makeGetNcmController()))
 
+router.get('/cfop', adaptOakRoute(makeListCfopController()))
+router.get('/cfop/:code', adaptOakRoute(makeGetCfopController()))
+
+
 router.get("/api/v1/hello", (context) => {
-  
+
   context.response.status = 500
   context.response.body = {
     success: true,
--- tests/listNcm_test.ts
+++ tests/listNcm_test.ts
@@ -1,10 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.156.0/testing/asserts.ts";
-import { superoak } from "https://deno.land/x/superoak/mod.ts";
-import { makeApp } from "../src/server.ts";
 import { makeNcmController } from "../src/usecases/ncm/index.ts";
 
-Deno.test("Should list all ncms", async() => {
+Deno.test("Should list all ncms", async () => {
   const controller = makeNcmController()
-  const resp = await controller.handle({}) 
+  const resp = await controller.handle({})
   assertEquals(resp.status, 200)
 });
